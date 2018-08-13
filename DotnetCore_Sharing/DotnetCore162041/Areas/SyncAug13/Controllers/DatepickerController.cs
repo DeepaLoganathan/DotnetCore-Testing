0cd4f2b7ea338c3d87b42ac7e24768101482e3d9
Deepa Loganathan <deepaloganathan@syncfusion.com>
2018-08-13 15:07:26 +0530

    test

---
+++ DotnetCore_Sharing/DotnetCore162041/Areas/SyncAug13/Controllers/DatepickerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Syncfusion.EJ2;
+
+namespace DotnetCore162041.SyncAug13.Controllers
+{
+    [Area("SyncAug13")]
+    public class DatepickerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
